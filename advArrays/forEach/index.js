d153c2f21f3ca92d1f4e8da86b50a0380c4dec78
Daniel Hill <daniel.hill13@gmail.com>
2019-10-14 16:39:48 -0500

    foreach time

--- advArrays/forEach/index.js
+++ advArrays/forEach/index.js
@@ -1,6 +1,20 @@
 //forEarch always returns undefined
 
+// array.forEach(function(value, index, array){
+//     //callback executed on every value in the array
+// })
 
+// var arr = [1,2,3];
+// arr.forEach(function(value, index, array){
+//     console.log(value + " " + index + " " + array);
+// })
 
-
-
+function halfValues(arr){
+    var newArr = [];
+    arr.forEach(function(val){
+        newArr.push(val / 2);
+    })
+    console.log(newArr)
+    return newArr;
+};
+halfValues([2,4,6,8,10]);
